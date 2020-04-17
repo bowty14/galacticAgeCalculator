049721d872193371ac876e04d4800ee5e54ba327
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-04-17 10:23:20 -0700

    create test to see that constructor is holding values of planets, should fail.

--- _tests_/review5.test.js
+++ _tests_/review5.test.js
@@ -5,5 +5,8 @@ describe('GalacticAge', () => {
   test('age constructor holds values', () => {
     let galacticage = new GalacticAge();
     expect(galacticage.mercury).toEqual(.24);
+    expect(galacticage.venus).toEqual(0);
+    expext(galacticage.mars).toEqual(0);
+    expect(galacticage.jupiter).toEqual(0);
   }); 
 });
